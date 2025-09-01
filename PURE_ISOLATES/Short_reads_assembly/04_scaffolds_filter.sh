0cff11a3f359a8747d2dac724f03167695fe912a
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-01 13:44:55 +0200

    Update 04_scaffolds_filter.sh

--- PURE_ISOLATES/Short_reads_assembly/04_scaffolds_filter.sh
+++ PURE_ISOLATES/Short_reads_assembly/04_scaffolds_filter.sh
@@ -2,16 +2,16 @@
 
 #SBATCH --partition cpu
 #SBATCH --job-name scaffolds_filtering
-#SBATCH --output /scratch/syersin2/Satellite_scratch/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/Satellite_scratch/std_output/%x_%j.err
+#SBATCH --output /scratch/<USER>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USER>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 8
 #SBATCH --mem 20G
 #SBATCH --time 00:30:00
-#SBATCH --array=9
+#SBATCH --array=1-XXX
 
-## Load modules
+## Load modules - adapt
 module load gcc/11.4.0
 module load python/3.7.17
 module load miniconda3/22.11.1
@@ -21,7 +21,7 @@ eval "$(conda shell.bash hook)"
 conda activate /work/FAC/FBM/DMF/pvonaesc/vonasch_lab_general/syersin/BioPython/biopython
 
 ## Variables
-filter_indir=/scratch/syersin2/Satellite_scratch/Isolates/spades
+filter_indir=/scratch/<USER>/<Project_scratch>/spades
 
 ## Array variables
 cd ${filter_indir}
@@ -29,8 +29,8 @@ sample_name=$(ls | sed -n ${SLURM_ARRAY_TASK_ID}p)
 
 
 ## Run contigs filtering
-python /users/syersin2/Satellite/scripts/Isolates/WGS/scaffold_filter.py ${sample_name} scaffolds ${filter_indir}/${sample_name}/scaffolds.fasta ${filter_indir}/${sample_name}/ ISO
-python /users/syersin2/Satellite/scripts/Isolates/WGS/scaffold_filter.py ${sample_name} contigs ${filter_indir}/${sample_name}/contigs.fasta ${filter_indir}/${sample_name}/ ISO
+python /users/<USER>/<Project_scratch>/scaffold_filter.py ${sample_name} scaffolds ${filter_indir}/${sample_name}/scaffolds.fasta ${filter_indir}/${sample_name}/ ISO
+python /users/<USER>/<Project_scratch>/scaffold_filter.py ${sample_name} contigs ${filter_indir}/${sample_name}/contigs.fasta ${filter_indir}/${sample_name}/ ISO
 
 ## Get assembly stats
 assembly-stats -l 200 -t <(cat ${filter_indir}/${sample_name}/${sample_name}.scaffolds.min200.fasta) > ${filter_indir}/${sample_name}/${sample_name}.min200.assembly.stats
