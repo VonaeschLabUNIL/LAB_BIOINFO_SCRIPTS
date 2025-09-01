67f5960ac422077f719d489b2965cbaeb45853f2
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-01 13:10:29 +0200

    Update 09_drep.sh

--- IN_HUMANS/METAGENOMIC/C_MAGs_assembly/09_drep.sh
+++ IN_HUMANS/METAGENOMIC/C_MAGs_assembly/09_drep.sh
@@ -2,15 +2,17 @@
 
 #SBATCH --partition cpu
 #SBATCH --job-name drep_mags
-#SBATCH --output /scratch/syersin2/Satellite_scratch/transmission_dir/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/Satellite_scratch/transmission_dir/std_output/%x_%j.err
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 32
 #SBATCH --mem 8G
 #SBATCH --time 12:00:00
 
-# Module
+# Optional step: script to dereplicate MAGs to a taxonomic level usign dRep
+
+# Module - adapt
 module load gcc/11.4.0
 module load miniconda3/22.11.1
 
@@ -20,9 +22,9 @@ conda activate /work/FAC/FBM/DMF/pvonaesc/vonasch_lab_general/syersin/dRep/drep
 
 # Variables - set your directories
 # The MAGs directory (mags_dir) is all the MAGs to be dereplicated in one folder
-drep_dir=/scratch/syersin2/Satellite_scratch/transmission_dir/dereplication
-mags_dir=/scratch/syersin2/Satellite_scratch/transmission_dir/MAGs
-genome_info=/scratch/syersin2/Satellite_scratch/transmission_dir/tables
+drep_dir=/scratch/<USERS>/<Project_scratch>/dereplication
+mags_dir=/scratch/<USERS>/<Project_scratch>/MAGs
+genome_info=/scratch/<USERS>/<Project_scratch>/tables
 
 # Dereplicate 
 # Adapt -pa (primary threshold for 1st clutser) and -sa (secondary threshold for 2nd clusters)
