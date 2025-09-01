ae0f1852d05761e664cb1bcf876a111637f9548f
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-01 13:13:39 +0200

    Update 03_Clade_name.sh

--- IN_HUMANS/METAGENOMIC/StrainPhlan/03_Clade_name.sh
+++ IN_HUMANS/METAGENOMIC/StrainPhlan/03_Clade_name.sh
@@ -2,23 +2,21 @@
 
 ##################################################################
 # Extracting clade names from the marker files
-# Margaux Creze
+# Written by Margaux Creze
 # 18.12.2024
 ##################################################################
 
 #SBATCH --partition cpu
 #SBATCH --job-name 03_Clade_name
-#SBATCH --output /scratch/mcreze/StrainPhlan_scratch/std_output/%x_%A_%j.out
-#SBATCH --error /scratch/mcreze/StrainPhlan_scratch/std_output/%x_%A_%j.err
-#SBATCH --mail-type BEGIN,END,FAIL,TIME_LIMIT_80
-#SBATCH --mail-user margaux.creze@unil.ch
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%A_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%A_%j.err
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 10
 #SBATCH --mem 20G
 #SBATCH --time 4:00:00
 
-# Module
+# Module - adapt
 module load gcc/11.4.0
 module load miniconda3/22.11.1
 
@@ -27,7 +25,7 @@ eval "$(conda shell.bash hook)"
 conda activate /work/FAC/FBM/DMF/pvonaesc/vonasch_lab_general/syersin/MetaPhlan/metaphlan
 
 ## Variables
-indir=/scratch/mcreze/StrainPhlan_scratch/Consensus_markers
+indir=/scratch/<USERS>/<Project_scratch>/Consensus_markers
 
 ## extracting clade names
 strainphlan -s ${indir}/*.json.bz2 \
