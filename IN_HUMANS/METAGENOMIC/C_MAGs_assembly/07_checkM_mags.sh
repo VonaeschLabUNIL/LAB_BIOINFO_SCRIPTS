b1bac8efa7281fd79a1bb7728089adc10d5faae5
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-01 13:08:32 +0200

    Update 07_checkM_mags.sh

--- IN_HUMANS/METAGENOMIC/C_MAGs_assembly/07_checkM_mags.sh
+++ IN_HUMANS/METAGENOMIC/C_MAGs_assembly/07_checkM_mags.sh
@@ -2,17 +2,17 @@
 
 #SBATCH --partition cpu
 #SBATCH --job-name CheckM_MAGs
-#SBATCH --output /scratch/syersin2/mags_scratch/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/mags_scratch/std_output/%x_%j.err
-#SBATCH --mail-type BEGIN,END,FAIL,TIME_LIMIT_80
-#SBATCH --mail-user simon.yersin@unil.ch
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 16 
 #SBATCH --mem 200G
 #SBATCH --time 01:00:00
 
-# Module
+# Script to check quality of the MAGs using checkM
+
+# Module - adapt
 module load gcc/10.4.0
 module load miniconda3/4.10.3
 
@@ -21,7 +21,7 @@ eval "$(conda shell.bash hook)"
 conda activate /work/FAC/FBM/DMF/pvonaesc/vonasch_lab_general/syersin/checkM/checkm
 
 # Variables
-qc_indir=/scratch/syersin2/mags_scratch/mags_output
+qc_indir=/scratch/<USERS>/<Project_scratch>/mags_output
 
 # Run checkm
 mkdir -p ${qc_indir}/checkM
@@ -32,7 +32,3 @@ checkm lineage_wf ${qc_indir}/MAGs \
     -t 16 \
     -f ${qc_indir}/checkM_stats.tsv \
     --tab_table
-
-mkdir -p users/syersin2/mags_analysis/checkM
-cp ${qc_indir}/checkM_stats.tsv users/syersin2/mags_analysis/checkM
-cp ${qc_indir}/check.log users/syersin2/mags_analysis/checkM
