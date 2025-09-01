d916d1847e25006deff0ce974edfcdb174256272
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-01 11:56:37 +0200

    Update and rename 0_preprocessing.sh to 00_preprocessing.sh

--- IN_HUMANS/METAGENOMIC/A_preprocessing/00_preprocessing.sh
+++ IN_HUMANS/METAGENOMIC/A_preprocessing/00_preprocessing.sh
@@ -2,16 +2,16 @@
 
 #SBATCH --partition cpu
 #SBATCH --job-name integrity
-#SBATCH --error /scratch/syersin2/std_output/%x_%j.err
-#SBATCH --output /scratch/syersin2/std_output/%x_%j.out
+#SBATCH --error /scratch/*USERS*/std_output/%x_%j.err
+#SBATCH --output /scratch/*USERS*/std_output/%x_%j.out
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 8
 #SBATCH --time 00:05:00
-#SBATCH --array=1-346
+#SBATCH --array=1-XX
 
-indir=/scratch/syersin2/RawData
-outdir=/scratch/syersin2/integrity
+indir=/scratch/*USERS*/RawData
+outdir=/scratch/*USERS*/integrity
 
 cd ${indir}
 sample=$(ls | sed -n ${SLURM_ARRAY_TASK_ID}p)
