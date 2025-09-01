06f21ad0dbba47343df1e9d6abdbcd2e904f9c8b
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-01 13:02:31 +0200

    Update 03_contig_filter.sh

--- IN_HUMANS/METAGENOMIC/C_MAGs_assembly/03_contig_filter.sh
+++ IN_HUMANS/METAGENOMIC/C_MAGs_assembly/03_contig_filter.sh
@@ -2,16 +2,16 @@
 
 #SBATCH --partition cpu
 #SBATCH --job-name contigs_filtering
-#SBATCH --output /scratch/syersin2/mags_scratch/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/mags_scratch/std_output/%x_%j.err
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 8
 #SBATCH --mem 2G
 #SBATCH --time 01:00:00
-#SBATCH --array=1-3
+#SBATCH --array=1-XXX
 
-## Load modules
+## Load modules - adapt
 module load gcc/10.4.0
 module load python/3.7.10
 module load miniconda3/4.10.3
@@ -21,9 +21,8 @@ eval "$(conda shell.bash hook)"
 conda activate /work/FAC/FBM/DMF/pvonaesc/vonasch_lab_general/syersin/BioPython/biopython
 
 ## Variables
-filter_workdir=/scratch/syersin2/mags_scratch
-filter_indir=/scratch/syersin2/mags_scratch/output_data/metaspades
-keep_dir=/users/syersin2/mags_test/output
+filter_workdir=/scratch/<USERS>/<Project_scratch>
+filter_indir=/scratch/<USERS>/<Project_scratch>/output_data/metaspades
 
 ## Array variables
 cd ${filter_indir}
@@ -37,11 +36,3 @@ python /users/syersin2/mags_test/scripts/scaffold_filter.py ${sample_name} conti
 ## Get assembly stats
 assembly-stats -l 500 -t <(cat ${filter_indir}/${sample_name}/${sample_name}.scaffolds.min500.fasta) > ${filter_indir}/${sample_name}/${sample_name}.min500.assembly.stats
 assembly-stats -l 1000 -t <(cat ${filter_indir}/${sample_name}/${sample_name}.scaffolds.min1000.fasta) > ${filter_indir}/${sample_name}/${sample_name}.min1000.assembly.stats
-
-## Copy important files to /users directory
-cp ${filter_indir}/${sample_name}/${sample_name}.min500.assembly.stats ${keep_dir}/${sample_name}
-cp ${filter_indir}/${sample_name}/${sample_name}.min1000.assembly.stats ${keep_dir}/${sample_name}
-cp ${filter_indir}/${sample_name}/${sample_name}.scaffolds.min500.fasta ${keep_dir}/${sample_name}
-cp ${filter_indir}/${sample_name}/${sample_name}.scaffolds.min1000.fasta ${keep_dir}/${sample_name}
-
-gzip ${keep_dir}/${sample_name}/*.fasta
