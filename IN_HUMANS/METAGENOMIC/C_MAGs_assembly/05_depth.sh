102ea070455cd7193e04fc3cd320a922a5dbb8e9
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-01 13:06:20 +0200

    Update 05_depth.sh

--- IN_HUMANS/METAGENOMIC/C_MAGs_assembly/05_depth.sh
+++ IN_HUMANS/METAGENOMIC/C_MAGs_assembly/05_depth.sh
@@ -2,16 +2,16 @@
 
 #SBATCH --partition cpu
 #SBATCH --job-name depth
-#SBATCH --output /scratch/syersin2/mags_scratch/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/mags_scratch/std_output/%x_%j.err
-#SBATCH --mail-type BEGIN,END,FAIL,TIME_LIMIT_80
-#SBATCH --mail-user simon.yersin@unil.ch
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 16
 #SBATCH --mem 10G
 #SBATCH --time 04:00:00
-#SBATCH --array=1-3
+#SBATCH --array=1-XXX
+
+# Script to obtain depth of backmapping using metabat2
 
 ## Load module
 module load gcc/10.4.0
@@ -22,7 +22,7 @@ eval "$(conda shell.bash hook)"
 conda activate /work/FAC/FBM/DMF/pvonaesc/vonasch_lab_general/syersin/MetaBAT2/metabat2
 
 ## Variables
-depth_indir=/scratch/syersin2/mags_scratch/output_data/metaspades
+depth_indir=/scratch/<USERS>/<Project_scratch>/output_data/metaspades
 
 ## Array variables
 cd ${depth_indir}
@@ -38,8 +38,6 @@ echo "#####################################################"
 jgi_summarize_bam_contig_depths --outputDepth ${depth_indir}/${sample_name}/${sample_name}_depth/${sample_name}.depth \
     ${depth_indir}/${sample_name}/${sample_name}_backmapping/*.bam
 
-mkdir -p /users/syersin2/mags_analysis/${sample_name}/${sample_name}_depth
-cp ${depth_indir}/${sample_name}/${sample_name}_depth/*.depth /users/syersin2/mags_analysis/${sample_name}/${sample_name}_depth
 
 echo "#####################################################"
 echo $id " Finishing time : $(date -u)"
