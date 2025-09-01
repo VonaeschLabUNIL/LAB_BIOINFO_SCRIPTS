dad7fa387ec4f5216069c7a303ab9d316d9d60ef
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-01 13:14:48 +0200

    Update 05_StrainPhlan.sh

--- IN_HUMANS/METAGENOMIC/StrainPhlan/05_StrainPhlan.sh
+++ IN_HUMANS/METAGENOMIC/StrainPhlan/05_StrainPhlan.sh
@@ -2,24 +2,22 @@
 
 #####################################
 # Generate alignments and then trees
-# Margaux Creze
+# Written by Margaux Creze
 # 18.12.2024
 ######################################
 
 #SBATCH --partition cpu
 #SBATCH --job-name 05_StrainPhlan
-#SBATCH --output /scratch/mcreze/StrainPhlan_scratch/std_output/%x_%A_%j.out
-#SBATCH --error /scratch/mcreze/StrainPhlan_scratch/std_output/%x_%A_%j.err
-#SBATCH --mail-type BEGIN,END,FAIL,TIME_LIMIT_80
-#SBATCH --mail-user margaux.creze@unil.ch
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%A_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%A_%j.err
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 10
 #SBATCH --mem 20G
 #SBATCH --time 01:00:00
-#SBATCH --array 1-2 #check this line to know how many clades in clade.txt : Thu Dec 19 15:05:41 2024: Detected 2 clades: 
+#SBATCH --array 1-XXX #check this line to know how many clades in clade.txt : Thu Dec 19 15:05:41 2024: Detected 2 clades: 
 
-# Module
+# Module - adapt
 module load gcc/11.4.0
 module load miniconda3/22.11.1
 module load samtools
@@ -32,9 +30,9 @@ eval "$(conda shell.bash hook)"
 conda activate /work/FAC/FBM/DMF/pvonaesc/vonasch_lab_general/syersin/MetaPhlan/metaphlan
 
 ## Variables
-indir=/scratch/mcreze/StrainPhlan_scratch/Consensus_markers
-outdir_clade=/scratch/mcreze/StrainPhlan_scratch/CladeMarkers
-outdir_sp=/scratch/mcreze/StrainPhlan_scratch/StrainPhlan_output
+indir=/scratch/<USERS>/<Project_scratch>Consensus_markers
+outdir_clade=/scratch/<USERS>/<Project_scratch>/CladeMarkers
+outdir_sp=/scratch/<USERS>/<Project_scratch>/StrainPhlan_output
 
 mkdir -p ${outdir_sp}
 
