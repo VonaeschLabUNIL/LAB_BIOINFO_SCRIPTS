686794d4d03d81e8cddc2920b499d5db4b49de76
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-01 13:46:40 +0200

    Update 05_1_copy_assembly.sh

--- PURE_ISOLATES/Short_reads_assembly/05_1_copy_assembly.sh
+++ PURE_ISOLATES/Short_reads_assembly/05_1_copy_assembly.sh
@@ -2,17 +2,17 @@
 
 #SBATCH --partition cpu
 #SBATCH --job-name copy_assemblies
-#SBATCH --output /scratch/syersin2/Satellite_scratch/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/Satellite_scratch/std_output/%x_%j.err
+#SBATCH --output /scratch/<USER>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USER>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 8
 #SBATCH --time 00:50:00
-#SBATCH --array=1-9
+#SBATCH --array=1-XXX
 
 # Variables
-indir=/scratch/syersin2/Satellite_scratch/Isolates/spades
-outdir=/scratch/syersin2/Satellite_scratch/Isolates/wgs_assemblies
+indir=/scratch/<USER>/<Project_scratch>/spades
+outdir=/scratch/<USER>/<Project_scratch>/wgs_assemblies
 
 ## Array variables
 cd ${indir}
