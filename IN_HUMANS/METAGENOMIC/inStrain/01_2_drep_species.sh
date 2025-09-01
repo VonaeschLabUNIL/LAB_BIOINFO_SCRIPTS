1d3dd45c18674b25b7f0404b8048a2ea9c745594
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-01 13:19:23 +0200

    Update 01_2_drep_species.sh

--- IN_HUMANS/METAGENOMIC/inStrain/01_2_drep_species.sh
+++ IN_HUMANS/METAGENOMIC/inStrain/01_2_drep_species.sh
@@ -2,15 +2,17 @@
 
 #SBATCH --partition cpu
 #SBATCH --job-name drep_mags_95
-#SBATCH --output /scratch/syersin2/Satellite_scratch/transmission_dir/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/Satellite_scratch/transmission_dir/std_output/%x_%j.err
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 32
 #SBATCH --mem 8G
 #SBATCH --time 12:00:00
 
-# Module
+# Script to dereplicate MAGs at the species level using dRep
+
+# Module - adapt
 module load gcc/11.4.0
 module load miniconda3/22.11.1
 
@@ -19,9 +21,9 @@ eval "$(conda shell.bash hook)"
 conda activate /work/FAC/FBM/DMF/pvonaesc/vonasch_lab_general/syersin/dRep/drep
 
 # Variables
-drep_dir=/scratch/syersin2/Satellite_scratch/transmission_dir/dereplication_species
-mags_dir=/scratch/syersin2/Satellite_scratch/transmission_dir/MAGs
-genome_info=/scratch/syersin2/Satellite_scratch/transmission_dir/tables
+drep_dir=/scratch/<USERS>/<Project_scratch>/dereplication_species
+mags_dir=/scratch/<USERS>/<Project_scratch>/MAGs
+genome_info=/scratch/<USERS>/<Project_scratch>/tables
 
 # Dereplicate 
 dRep dereplicate ${drep_dir} \
