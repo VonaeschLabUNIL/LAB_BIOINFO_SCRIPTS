ff0fc62e6ecd1cc1fc9cd5d1a2bdc3f6be517058
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-01 13:14:09 +0200

    Update 04_Extract_markers.sh

--- IN_HUMANS/METAGENOMIC/StrainPhlan/04_Extract_markers.sh
+++ IN_HUMANS/METAGENOMIC/StrainPhlan/04_Extract_markers.sh
@@ -2,16 +2,14 @@
 
 ##############################################
 # Extract the markers from MetaPhlAn database
-# Margaux Creze
+# Written by Margaux Creze
 # 18.12.2024
 ##############################################
 
 #SBATCH --partition cpu
 #SBATCH --job-name 04_Extract_markers
-#SBATCH --output /scratch/mcreze/StrainPhlan_scratch/std_output/%x_%A_%j.out
-#SBATCH --error /scratch/mcreze/StrainPhlan_scratch/std_output/%x_%A_%j.err
-#SBATCH --mail-type BEGIN,END,FAIL,TIME_LIMIT_80
-#SBATCH --mail-user margaux.creze@unil.ch
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%A_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%A_%j.err
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 10
@@ -20,7 +18,7 @@
 #SBATCH --array 1-2 #check this line to know how many clades in clade.txt : Thu Dec 19 15:05:41 2024: Detected 2 clades: 
 
 
-# Module
+# Module - adapt
 module load gcc/11.4.0
 module load miniconda3/22.11.1
 
@@ -30,8 +28,8 @@ eval "$(conda shell.bash hook)"
 conda activate /work/FAC/FBM/DMF/pvonaesc/vonasch_lab_general/syersin/MetaPhlan/metaphlan
 
 ## Variables
-indir=/scratch/mcreze/StrainPhlan_scratch/Consensus_markers
-outdir_clade=/scratch/mcreze/StrainPhlan_scratch/CladeMarkers
+indir=/scratch/<USERS>/<Project_scratch>/Consensus_markers
+outdir_clade=/scratch/<USERS>/<Project_scratch>/CladeMarkers
 
 mkdir -p ${outdir_clade}
 
