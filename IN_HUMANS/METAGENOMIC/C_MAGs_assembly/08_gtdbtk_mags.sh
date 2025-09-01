562f834592d672572788c755691532c204083eb2
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-01 13:09:25 +0200

    Update 08_gtdbtk_mags.sh

--- IN_HUMANS/METAGENOMIC/C_MAGs_assembly/08_gtdbtk_mags.sh
+++ IN_HUMANS/METAGENOMIC/C_MAGs_assembly/08_gtdbtk_mags.sh
@@ -5,11 +5,13 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=150G
 #SBATCH --job-name=GTDBTK
-#SBATCH --output /scratch/syersin2/mags_scratch/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/mags_scratch/std_output/%x_%j.err
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --time 4:00:00
 
-# Module
+# Script to assign taxonomy to the MAGs using GTDB-tk
+
+# Modules - adapt
 module load gcc/10.4.0
 module load miniconda3/4.10.3
 
@@ -18,7 +20,7 @@ eval "$(conda shell.bash hook)"
 conda activate /work/FAC/FBM/DMF/pvonaesc/vonasch_lab_general/syersin/GTDB_TK/gtdbtk
 
 #### Create directories and define variables
-gtdb_indir=/scratch/syersin2/mags_scratch/mags_output
+gtdb_indir=/scratch/<USERS>/<Project_scratch>/mags_output
 
 ## Create output directory
 mkdir -p ${gtdb_indir}/GTDB
@@ -29,8 +31,3 @@ gtdbtk classify_wf --genome_dir ${gtdb_indir}/MAGs \
     --mash_db ${gtdb_indir}/GTDB/gtdbtk.msh \
     --extension fa \
     --cpus 16
-
-mkdir -p users/syersin2/mags_analysis/GTDB
-cp ${qc_indir}/gtdbtk.bac120.summary.tsv users/syersin2/mags_analysis/GTDB
-cp ${qc_indir}/gtdbtk.log users/syersin2/mags_analysis/GTDB
-cp ${qc_indir}/gtdbtk.warnings.log users/syersin2/mags_analysis/GTDB
