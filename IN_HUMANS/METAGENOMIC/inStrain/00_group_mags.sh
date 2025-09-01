5c712dc5e7f0ab1b66fbdbb591890fb0fa8e5fb8
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-01 13:17:51 +0200

    Update 00_group_mags.sh

--- IN_HUMANS/METAGENOMIC/inStrain/00_group_mags.sh
+++ IN_HUMANS/METAGENOMIC/inStrain/00_group_mags.sh
@@ -2,20 +2,22 @@
 
 #SBATCH --partition cpu
 #SBATCH --job-name group_mags
-#SBATCH --output /scratch/syersin2/Satellite_scratch/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/Satellite_scratch/std_output/%x_%j.err
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 16
 #SBATCH --mem 8G
 #SBATCH --time 00:10:00
-#SBATCH --array=1-129
+#SBATCH --array=1-XX
+
+# If necessary - script to group all MAGs into a single folder
 
 ## Variables
-files=/scratch/syersin2/Satellite_scratch/transmission_dir/MAGs_per_samples
+files=/scratch/<USERS>/<Project_scratch>/MAGs_per_samples
 
 ## Array variables
 cd ${files}
 sample_name=$(ls | sed -n ${SLURM_ARRAY_TASK_ID}p)
 
-cp ${files}/${sample_name}/*.fa.gz /scratch/syersin2/Satellite_scratch/transmission_dir/MAGs
+cp ${files}/${sample_name}/*.fa.gz /scratch/<USERS>/<Project_scratch>/MAGs
