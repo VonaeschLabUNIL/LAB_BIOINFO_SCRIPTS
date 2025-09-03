460b403563e0bedba8d77ffc36cd5c6eb4457e69
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-03 16:21:07 +0200

    Update 04_mmseqs_db.sh

--- IN_HUMANS/METAGENOMIC/D_Gene_annotations/A_Building/04_mmseqs_db.sh
+++ IN_HUMANS/METAGENOMIC/D_Gene_annotations/A_Building/04_mmseqs_db.sh
@@ -2,27 +2,27 @@
 
 #SBATCH --partition cpu
 #SBATCH --job-name mmseqs_db
-#SBATCH --output /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.err
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 2
 #SBATCH --mem 10G
 #SBATCH --time 00:10:00
 
-# Module
+# Modules - adapt
 module load gcc/12.3.0
 module load mmseqs2/14-7e284
 
 # Variable
-indir=/scratch/syersin2/Afribiota_scratch/catalogue/prodigal
-outdir=/scratch/syersin2/Afribiota_scratch/catalogue/mmseqs9590
+indir=/scratch/<USERS>/<Project_scratch>/catalogue/prodigal
+outdir=/scratch/<USERS>/<Project_scratch>/catalogue/mmseqs9590
 
 mkdir -p ${outdir}
 
 # dbtyp 2 is for amino acid
-mmseqs createdb ${indir}/Afribiota_gene_catalog_all.fna.gz \
-    ${outdir}/Afribiota_gene_catalog_all.mmseqs.db \
+mmseqs createdb ${indir}/Gene_catalog_all.fna.gz \
+    ${outdir}/Gene_catalog_all.mmseqs.db \
     --dbtype 2 --shuffle 0
 
 # From output:
