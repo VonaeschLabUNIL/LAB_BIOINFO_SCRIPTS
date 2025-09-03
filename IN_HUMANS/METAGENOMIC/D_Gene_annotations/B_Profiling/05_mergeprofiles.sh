46b8d27534309d1eb2facaa177ce11b35bbf8159
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-03 16:28:12 +0200

    Update 05_mergeprofiles.sh

--- IN_HUMANS/METAGENOMIC/D_Gene_annotations/B_Profiling/05_mergeprofiles.sh
+++ IN_HUMANS/METAGENOMIC/D_Gene_annotations/B_Profiling/05_mergeprofiles.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=mergeprof
-#SBATCH --output /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.err
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=8G
 #SBATCH --time=04:00:00
 
-# Load necessary modules
+# Load necessary modules - adapt
 module load gcc/12.3.0
 module load miniforge3/4.8.3-4-Linux-x86_64
 
@@ -16,8 +16,8 @@ eval "$(conda shell.bash hook)"
 conda activate /work/FAC/FBM/DMF/pvonaesc/vonasch_lab_general/syersin/SushiCounter/counter
 
 ## Variables
-indir=/scratch/syersin2/Afribiota_scratch/catalogue/profiles
-outdir=/scratch/syersin2/Afribiota_scratch/catalogue/profiles/merged
+indir=/scratch/<USERS>/<Project_scratch>/catalogue/profiles
+outdir=/scratch/<USERS>/<Project_scratch>/catalogue/profiles/merged
 mkdir -p ${outdir}
 
-sushicounter mergeprofiles -i ${indir}/*.genecount.profile -o ${outdir}/Afribiota_
+sushicounter mergeprofiles -i ${indir}/*.genecount.profile -o ${outdir}/Project_
