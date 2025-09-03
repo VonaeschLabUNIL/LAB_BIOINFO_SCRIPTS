412c1190220b723e7202ff172e4cbac5d896cead
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-03 16:24:49 +0200

    Update 09_faa_seqtk.sh

--- IN_HUMANS/METAGENOMIC/D_Gene_annotations/A_Building/09_faa_seqtk.sh
+++ IN_HUMANS/METAGENOMIC/D_Gene_annotations/A_Building/09_faa_seqtk.sh
@@ -2,25 +2,25 @@
 
 #SBATCH --partition cpu
 #SBATCH --job-name seqtkfna
-#SBATCH --output /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.err
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 2
 #SBATCH --mem 20G
 #SBATCH --time 04:00:00
 
-# Module
+# Modules - adapt
 module load gcc/12.3.0
 module load seqtk/1.4
 
 ## Variables
-indir=/scratch/syersin2/Afribiota_scratch/catalogue/prodigal
-mmseqsdir=/scratch/syersin2/Afribiota_scratch/catalogue/mmseqs9590
-outdir=/scratch/syersin2/Afribiota_scratch/catalogue/derepcat
+indir=/scratch/<USERS>/<Project_scratch>/catalogue/prodigal
+mmseqsdir=/scratch/<USERS>/<Project_scratch>/catalogue/mmseqs9590
+outdir=/scratch/<USERS>/<Project_scratch>/catalogue/derepcat
 
 mkdir -p ${outdir}
 
-zcat ${indir}/Afribiota_gene_catalog_all.faa | \
+zcat ${indir}/Gene_catalog_all.faa | \
     seqtk subseq - ${mmseqsdir}/mmseqs9590.headers \
-    > ${outdir}/Afribiota_gene_catalog_derep.faa
+    > ${outdir}/Gene_catalog_derep.faa
