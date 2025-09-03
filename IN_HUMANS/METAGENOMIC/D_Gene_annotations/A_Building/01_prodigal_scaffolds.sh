1c54daf7d258186d9393377df33d51eec52aa42c
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-03 16:18:44 +0200

    Update 01_prodigal_scaffolds.sh

--- IN_HUMANS/METAGENOMIC/D_Gene_annotations/A_Building/01_prodigal_scaffolds.sh
+++ IN_HUMANS/METAGENOMIC/D_Gene_annotations/A_Building/01_prodigal_scaffolds.sh
@@ -2,22 +2,22 @@
 
 #SBATCH --partition cpu
 #SBATCH --job-name prodigal
-#SBATCH --output /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.err
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 300M
 #SBATCH --time 01:00:00
-#SBATCH --array=1-57
+#SBATCH --array=1-XXX
 
-# Module
+# Modules - adapt
 module load gcc/12.3.0
 module load prodigal/2.6.3
 
 ## Variables
-indir=/scratch/syersin2/Afribiota_scratch/metaspades
-outdir=/scratch/syersin2/Afribiota_scratch/prodigal
+indir=/scratch/<USERS>/<Project_scratch>/metaspades
+outdir=/scratch/<USERS>/<Project_scratch>/prodigal
 
 sample=$(ls ${indir} | sed -n ${SLURM_ARRAY_TASK_ID}p)
 
