acaceb82ccaf387bb1d090872e2096e1a3423a8d
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-03 16:29:58 +0200

    Update 07_normalize.sh

--- IN_HUMANS/METAGENOMIC/D_Gene_annotations/B_Profiling/07_normalize.sh
+++ IN_HUMANS/METAGENOMIC/D_Gene_annotations/B_Profiling/07_normalize.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 #SBATCH --job-name=normfmg
-#SBATCH --output /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.err
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=100M
 #SBATCH --time=00:10:00
 
-# Load necessary modules
+# Load necessary modules - adapt
 module load python/3.12.1
 
 ## Variables
-indir=/scratch/syersin2/Afribiota_scratch/catalogue/profiles/merged
-mgdir=/scratch/syersin2/Afribiota_scratch/catalogue/fmg
-logdir=/scratch/syersin2/Afribiota_scratch/catalogue/log
-scriptdir=/users/syersin2/Afribiota/PEData/scripts/D_gene_catalog
+indir=/scratch/<USERS>/<Project_scratch>/catalogue/profiles/merged
+mgdir=/scratch/<USERS>/<Project_scratch>/catalogue/fmg
+logdir=/scratch/<USERS>/<Project_scratch>/catalogue/log
+scriptdir=/users/<USERS>/<Project_scratch>/scripts/D_gene_catalog
 
 python ${scriptdir}/normalise_by_motus_v3.fmg.py \
     ${mgdir} \
-    ${indir}/Afribiota__insertcounts.lengthnorm.profile \
-    > ${logdir}/Afribiota__insertcounts.lengthnorm.profile.cellab.log
+    ${indir}/Project_name__insertcounts.lengthnorm.profile \
+    > ${logdir}/Project_name__insertcounts.lengthnorm.profile.cellab.log
