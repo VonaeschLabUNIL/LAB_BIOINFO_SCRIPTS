daf37661ac071378669158ad324e0c6fe65c9981
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-03 16:20:17 +0200

    Update 03_merge_catalog.sh

--- IN_HUMANS/METAGENOMIC/D_Gene_annotations/A_Building/03_merge_catalog.sh
+++ IN_HUMANS/METAGENOMIC/D_Gene_annotations/A_Building/03_merge_catalog.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH --partition cpu
-#SBATCH --job-name zip
-#SBATCH --output /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.err
+#SBATCH --job-name merge
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 2
@@ -11,22 +11,19 @@
 #SBATCH --time 05:00:00
 
 ## Variables
-indir=/scratch/syersin2/Afribiota_scratch/prodigal
-outdir=/scratch/syersin2/Afribiota_scratch/catalogue/prodigal
+indir=/scratch/<USERS>/<Project_scratch>/prodigal
+outdir=/scratch/<USERS>/<Project_scratch>/catalogue/prodigal
 
 mkdir -p ${outdir}
 
-# for f in ${indir}/*.fna; do
-#     awk 'BEGIN{RS=">"; ORS=""} NR>1{print ">"$0"\n"}' "$f"
-# done > ${outdir}/Afribiota_gene_catalog_all.fna
+for f in ${indir}/*.fna; do
+     awk 'BEGIN{RS=">"; ORS=""} NR>1{print ">"$0"\n"}' "$f"
+done > ${outdir}/Afribiota_gene_catalog_all.fna
 
-#gzip ${outdir}/Afribiota_gene_catalog_all.fna
+gzip ${outdir}/Afribiota_gene_catalog_all.fna
 
-# for f in ${indir}/*.faa; do
-#     awk 'BEGIN{RS=">"; ORS=""} NR>1{print ">"$0"\n"}' "$f"
-# done > ${outdir}/Afribiota_gene_catalog_all.faa
+for f in ${indir}/*.faa; do
+     awk 'BEGIN{RS=">"; ORS=""} NR>1{print ">"$0"\n"}' "$f"
+done > ${outdir}/Afribiota_gene_catalog_all.faa
 
-#gzip ${outdir}/Afribiota_gene_catalog_all.faa
-
-gzip ${indir}/*.faa
-gzip ${indir}/*.fna
+gzip ${outdir}/Afribiota_gene_catalog_all.faa
