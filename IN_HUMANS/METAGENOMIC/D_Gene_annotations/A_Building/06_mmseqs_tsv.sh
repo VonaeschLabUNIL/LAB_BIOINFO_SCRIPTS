f73e3bf9d1a4f874b4d443f32f98f60c0ad7bce6
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-03 16:23:17 +0200

    Update 06_mmseqs_tsv.sh

--- IN_HUMANS/METAGENOMIC/D_Gene_annotations/A_Building/06_mmseqs_tsv.sh
+++ IN_HUMANS/METAGENOMIC/D_Gene_annotations/A_Building/06_mmseqs_tsv.sh
@@ -2,22 +2,22 @@
 
 #SBATCH --partition cpu
 #SBATCH --job-name mmseqs_tsv
-#SBATCH --output /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.err
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 4
 #SBATCH --mem 2G
 #SBATCH --time 00:30:00
 
-# Module
+# Modules - adapt
 module load gcc/12.3.0
 module load mmseqs2/14-7e284
 
 # Variable
-outdir=/scratch/syersin2/Afribiota_scratch/catalogue/mmseqs9590
+outdir=/scratch/<USERS>/<Project_scratch>/catalogue/mmseqs9590
 
-mmseqs createtsv ${outdir}/Afribiota_gene_catalog_all.mmseqs.db \
-    ${outdir}/Afribiota_gene_catalog_all.mmseqs.db \
-    ${outdir}/Afribiota_gene_catalog_all.mmseqs.db.cluster \
-    ${outdir}/Afribiota_gene_catalog_all.mmseqs.db.tsv
+mmseqs createtsv ${outdir}/Gene_catalog_all.mmseqs.db \
+    ${outdir}/Gene_catalog_all.mmseqs.db \
+    ${outdir}/Gene_catalog_all.mmseqs.db.cluster \
+    ${outdir}/Gene_catalog_all.mmseqs.db.tsv
