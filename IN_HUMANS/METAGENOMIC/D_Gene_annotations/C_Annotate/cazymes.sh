121a84e69fc059e65df65c68833aebfdef1520d1
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-03 16:30:49 +0200

    Update cazymes.sh

--- IN_HUMANS/METAGENOMIC/D_Gene_annotations/C_Annotate/cazymes.sh
+++ IN_HUMANS/METAGENOMIC/D_Gene_annotations/C_Annotate/cazymes.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --partition cpu
 #SBATCH --job-name cazymes
-#SBATCH --output /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.err
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 16
@@ -20,10 +20,10 @@ eval "$(conda shell.bash hook)"
 conda activate /work/FAC/FBM/DMF/pvonaesc/vonasch_lab_general/syersin/dbCAN/dbcan
 
 ## Variables
-indir=/scratch/syersin2/Afribiota_scratch/catalogue/derepcat/split_cat/Afribiota_gene_catalog_derep.faa.split
-outdir=/scratch/syersin2/Afribiota_scratch/annotations/cazymes
+indir=/scratch/<USERS>/<Project_scratch>/catalogue/derepcat/split_cat/Afribiota_gene_catalog_derep.faa.split
+outdir=/scratch/<USERS>/<Project_scratch>/annotations/cazymes
 dbdir=/work/FAC/FBM/DMF/pvonaesc/vonasch_lab_general/syersin/dbCAN/database
-pyscript=/users/syersin2/Afribiota/PEData/scripts/D_gene_catalog/C_Annotate
+pyscript=/users/<USERS>/<Project_scratch>/scripts/D_gene_catalog/C_Annotate
 
 # catalogue part
 parts=$(ls ${indir} | sed -n ${SLURM_ARRAY_TASK_ID}p | sed 's/\.faa$//')
