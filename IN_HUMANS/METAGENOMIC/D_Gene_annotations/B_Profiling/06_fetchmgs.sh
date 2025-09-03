588f41989a57a8c058188674658fe8012c6e777f
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-03 16:28:48 +0200

    Update 06_fetchmgs.sh

--- IN_HUMANS/METAGENOMIC/D_Gene_annotations/B_Profiling/06_fetchmgs.sh
+++ IN_HUMANS/METAGENOMIC/D_Gene_annotations/B_Profiling/06_fetchmgs.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 #SBATCH --job-name=fetchMG
-#SBATCH --output /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.err
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=4G
 #SBATCH --time=02:00:00
 
-# Load necessary modules
+# Load necessary modules - adapt
 module load gcc/12.3.0
 module load perl/5.38.0
 
 ## Variables
 scriptdir=/work/FAC/FBM/DMF/pvonaesc/vonasch_lab_general/syersin/FetchMGs/fetchMGs.pl
-indir=/scratch/syersin2/Afribiota_scratch/catalogue/derepcat
-outdir=/scratch/syersin2/Afribiota_scratch/catalogue/fmg
+indir=/scratch/<USERS>/<Project_scratch>/catalogue/derepcat
+outdir=/scratch/<USERS>/<Project_scratch>/catalogue/fmg
 
 rm -r ${outdir}
 
-${scriptdir}/fetchMGs.pl -m extraction ${indir}/Afribiota_gene_catalog_derep.faa \
-    -d ${indir}/Afribiota_gene_catalog_derep.fna \
+${scriptdir}/fetchMGs.pl -m extraction ${indir}/Gene_catalog_derep.faa \
+    -d ${indir}/Gene_catalog_derep.fna \
     -i \
     -t 10 \
     -o ${outdir}
