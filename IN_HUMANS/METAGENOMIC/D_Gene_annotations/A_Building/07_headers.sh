c8e3cfba971ca54c1e76d8a7f2439b45ce690ff2
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-03 16:23:41 +0200

    Update 07_headers.sh

--- IN_HUMANS/METAGENOMIC/D_Gene_annotations/A_Building/07_headers.sh
+++ IN_HUMANS/METAGENOMIC/D_Gene_annotations/A_Building/07_headers.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --partition cpu
 #SBATCH --job-name headers
-#SBATCH --output /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.err
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 1
@@ -11,7 +11,7 @@
 #SBATCH --time 00:10:00
 
 ## Variables
-dir=/scratch/syersin2/Afribiota_scratch/catalogue/mmseqs9590
+dir=/scratch/<USERS>/<Project_scratch>/catalogue/mmseqs9590
 
 # Extract representative sequence list
-cut -f 1 ${dir}/Afribiota_gene_catalog_all.mmseqs.db.tsv | sort | uniq > ${dir}/mmseqs9590.headers
+cut -f 1 ${dir}/Gene_catalog_all.mmseqs.db.tsv | sort | uniq > ${dir}/mmseqs9590.headers
