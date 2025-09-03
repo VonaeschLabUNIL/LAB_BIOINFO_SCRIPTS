0d9ec2123f1bc861209fa4e76004af04cb5cc7ea
Simon Yersin <108127320+yersim@users.noreply.github.com>
2025-09-03 16:26:21 +0200

    Update 02_align.sh

--- IN_HUMANS/METAGENOMIC/D_Gene_annotations/B_Profiling/02_align.sh
+++ IN_HUMANS/METAGENOMIC/D_Gene_annotations/B_Profiling/02_align.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 #SBATCH --job-name=bwa_align
-#SBATCH --output /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.out
-#SBATCH --error /scratch/syersin2/Afribiota_scratch/std_output/%x_%j.err
+#SBATCH --output /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.out
+#SBATCH --error /scratch/<USERS>/<Project_scratch>/std_output/%x_%j.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=16G
 #SBATCH --time=06:00:00
-#SBATCH --array=2-57
+#SBATCH --array=1-XX
 
-# Load necessary modules
+# Load necessary modules - adapt
 module load gcc/12.3.0
 module load bwa/0.7.17
 module load samtools/1.19.2
 
 ## Variables
-catdir=/scratch/syersin2/Afribiota_scratch/catalogue/derepcat
-indir=/scratch/syersin2/Afribiota_scratch/bw_cleaned_reads
-outdir=/scratch/syersin2/Afribiota_scratch/catalogue/alignment
+catdir=/scratch/<USERS>/<Project_scratch>/catalogue/derepcat
+indir=/scratch/<USERS>/<Project_scratch>/bw_cleaned_reads
+outdir=/scratch/<USERS>/<Project_scratch>/catalogue/alignment
 
 ## Array variables
 sample=$(ls ${indir} | sed -n ${SLURM_ARRAY_TASK_ID}p)
@@ -25,11 +25,11 @@ mkdir -p ${outdir}/${sample}
 
 ## Align reads to gene catalog
 echo "Running BWA MEM for forward : " ${sample}
-bwa mem -a -t 8 ${catdir}/Afribiota_gene_catalog_derep.fna ${indir}/${sample}/${sample}_bowtie2_R1_001.fastq.gz \
+bwa mem -a -t 8 ${catdir}/Gene_catalog_derep.fna ${indir}/${sample}/${sample}_bowtie2_R1_001.fastq.gz \
     | samtools view -F 4 -bh - > ${outdir}/${sample}/${sample}_r1.bam
 
 echo "Running BWA MEM for reverse : " ${sample}
-bwa mem -a -t 8 ${catdir}/Afribiota_gene_catalog_derep.fna ${indir}/${sample}/${sample}_bowtie2_R2_001.fastq.gz \
+bwa mem -a -t 8 ${catdir}/Gene_catalog_derep.fna ${indir}/${sample}/${sample}_bowtie2_R2_001.fastq.gz \
     | samtools view -F 4 -bh - > ${outdir}/${sample}/${sample}_r2.bam
 
 echo "Alignment complete"
